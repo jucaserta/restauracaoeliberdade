3f240993bd54dd2164cd4bcc3b96021b7f1012d4
jucaserta <jucaserta@hotmail.com>
2025-12-21 16:12:49 -0300

    Update testimonial.jsx

--- src/components/pages/home/testimonial.jsx
+++ src/components/pages/home/testimonial.jsx
@@ -11,70 +11,66 @@ import 'swiper/css/autoplay'
 import Quote from '../../../assets/images/quote-right-solid.png'
 import Testimonials from '../../../jsons/testimonial.json'
 
-
 export default function Testimonial() {
-    return (
-        <section>
-            <div className="container">
-                <SectionHeader
-                    subheading="What Healthcare Experts Say"
-                    heading="What Professionals Say About Our Products"
-                    className="text-center mx-auto xl:w-[45%]"
-                />
+  return (
+    <section id="d2c_testimonial">
+      <div className="container">
+        <SectionHeader
+          subheading="Depoimentos"
+          heading="O que famílias relatam após o primeiro atendimento"
+          className="text-center mx-auto xl:w-[45%]"
+        />
 
-                <div
-                    className="xl:max-w-[80%] mx-auto bg-testimonial bg-center bg-cover p-5 md:p-10 lg:p-14 rounded-2xl"
-                    data-aos="fade-slide-up"
-                    data-aos-delay="800"
-                >
-                    <div className="p-5 md:p-10 bg-off-white/15 border-light-green/20 backdrop-blur-md rounded-xl">
-                        <Swiper
-                            modules={[Autoplay, EffectCube]}
-                            effect="cube"
-                            cubeEffect={{
-                                shadow: false,
-                                slideShadows: false,
-                                shadowOffset: 0,
-                                shadowScale: 0,
-                            }}
-                            autoplay={{ delay: 2000 }}
-                            loop={true}
-                            slidesPerView={1}
-                            spaceBetween={24}
-                            speed={1000}
-                        >
-                            {Testimonials.map((testimonial, index) => (
-                                <SwiperSlide key={index} className="space-y-3 text-center">
-                                    <div>
-                                        <img
-                                            src={Quote.src}
-                                            alt="Quote"
-                                            width={40}
-                                            height={40}
-                                        />
-                                    </div>
-                                    <div className="space-y-12">
-                                        <p className="text-xl text-off-white">{testimonial.quote}</p>
-                                        <div className="text-center space-y-2">
-                                            <img
-                                                src={testimonial.image}
-                                                alt={testimonial.name}
-                                                width={64}
-                                                height={64}
-                                                className="d2c_client_image mx-auto"
-                                            />
-                                            <h4 className="text-off-white">{testimonial.name}</h4>
-                                            <p className="text-sm capitalize text-off-white">
-                                                {testimonial.role}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </SwiperSlide>
-                            ))}
-                        </Swiper>
+        <div
+          className="xl:max-w-[80%] mx-auto bg-testimonial bg-center bg-cover p-5 md:p-10 lg:p-14 rounded-2xl"
+          data-aos="fade-slide-up"
+          data-aos-delay="800"
+        >
+          <div className="p-5 md:p-10 bg-off-white/15 border-light-green/20 backdrop-blur-md rounded-xl">
+            <Swiper
+              modules={[Autoplay, EffectCube]}
+              effect="cube"
+              cubeEffect={{
+                shadow: false,
+                slideShadows: false,
+                shadowOffset: 0,
+                shadowScale: 0,
+              }}
+              autoplay={{ delay: 3500 }}
+              loop={true}
+              slidesPerView={1}
+              spaceBetween={24}
+              speed={900}
+            >
+              {Testimonials.map((testimonial, index) => (
+                <SwiperSlide key={index} className="space-y-3 text-center">
+                  <div className="flex justify-center">
+                    <img src={Quote.src} alt="Aspas" width={40} height={40} />
+                  </div>
+
+                  <div className="space-y-10">
+                    <p className="text-xl text-off-white">{testimonial.quote}</p>
+
+                    <div className="text-center space-y-2">
+                      <img
+                        src={testimonial.image}
+                        alt={testimonial.name}
+                        width={64}
+                        height={64}
+                        className="d2c_client_image mx-auto"
+                      />
+                      <h4 className="text-off-white">{testimonial.name}</h4>
+                      <p className="text-sm capitalize text-off-white">
+                        {testimonial.role}
+                      </p>
                     </div>
-                </div>
-            </div>
-        </section>
-    )
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
