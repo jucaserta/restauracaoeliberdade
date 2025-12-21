cda4b13baa8b151b48007fc10c5d93fe8dc5b8ef
jucaserta <jucaserta@hotmail.com>
2025-12-21 19:15:23 -0300

    Update navbar.jsx

--- src/components/shared/navbar.jsx
+++ src/components/shared/navbar.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Logo from "../../assets/images/logo/logo.png";
+import Logo from "../../assets/images/logo.png";
 
 const PHONE_TEL = "5511986843656";
 const WHATSAPP =
@@ -196,4 +196,5 @@ export default function Navbar() {
     </header>
   );
 }
+
 
